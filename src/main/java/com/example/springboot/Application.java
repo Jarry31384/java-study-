a77a6bc7e4e9157d2115ae6ec66a80c74dbba0c0
Jarry31384 <96226212+Jarry31384@users.noreply.github.com>
2022-04-25 08:06:17 +0800

    Springboot学习

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//这是一个Springboot应用
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
